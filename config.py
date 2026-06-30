6ffa310f877d2644ab9d9db1915359da33ab5f2e
BTWDJ <thakurchetan094@gmail.com>
2026-07-01 01:48:06 +0530

    Deployment ready: dynamic env vars and cleanup

--- config.py
+++ config.py
@@ -3,11 +3,13 @@ import os
 
 load_dotenv()
 
-API_ID = 12380656
-API_HASH = "d927c13beaaf5110f25c505b7c071273"
-BOT_TOKEN = "8987193319:AAEWFpOED00_XWdDROwpZPaKEbWX6myBgkI"
+API_ID = int(os.getenv("API_ID", "12380656"))
+API_HASH = os.getenv("API_HASH", "d927c13beaaf5110f25c505b7c071273")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8987193319:AAEWFpOED00_XWdDROwpZPaKEbWX6myBgkI")
 
-MONGO_URI = "mongodb+srv://Aero:8wP1Y9ggrOf9Msl6@cluster0.l0pwxat.mongodb.net/?appName=Cluster0"
-CHECKER_API_URL = "https://Worker.xb1ns.com"
-OWNER_ID = 1817159548
-ADMIN_IDS = [6677260209]  # Add admin IDs here
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Aero:8wP1Y9ggrOf9Msl6@cluster0.l0pwxat.mongodb.net/?appName=Cluster0")
+CHECKER_API_URL = os.getenv("CHECKER_API_URL", "https://Worker.xb1ns.com")
+OWNER_ID = int(os.getenv("OWNER_ID", "1817159548"))
+
+_admin_ids_str = os.getenv("ADMIN_IDS", "6677260209")
+ADMIN_IDS = [int(x.strip()) for x in _admin_ids_str.split(",") if x.strip()]
